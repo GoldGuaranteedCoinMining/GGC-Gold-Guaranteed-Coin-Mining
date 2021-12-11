ab787a820dd0343ffa025470960ab5f9c34ed345
CapTaiNBO <35251264+boldoo1231@users.noreply.github.com>
2021-12-11 15:18:22 +0800

    Delete bep-20.sol

--- bep-20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.1;
-
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
-    contract GoldGuaranteedCoin is ERC20 {
-        constructor(uint256 initialSupply) public ERC20("GoldGuaranteedCoin","GGC"){
-            _mint(msg.sender, initialSupply);
-        }
-    }
